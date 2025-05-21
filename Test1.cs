01898f1860d8e0d5945ec1ee82950337e08cf07a
Marc Nomena <89481372+MarcNomena@users.noreply.github.com>
2025-05-21 11:30:16 +0200

    test

---
+++ Test1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace pipelines_dotnet_core
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int result = 2 + 3;
+            Assert.AreEqual(5, result);
+        }
+    }
+}
